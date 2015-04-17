a8e55f14655cb544cfe5925cd8f92d8d299c3e03
Matthew Wynn <m-wynn@live.com>
2015-04-16 19:01:52 -0500

    Display a string alongside the hangman guy

--- main.asm
+++ main.asm
@@ -1,12 +1,20 @@
 .data
-picture: .asciiz 	"_______\n|   |  \\|\n        |\n        |\n        |\n        |\n        |\n        |\n       ---\n",
-					"_______\n|   |  \\|\n    O   |\n        |\n        |\n        |\n        |\n        |\n       ---\n",
-					"_______\n|   |  \\|\n    O   |\n    |   |\n    |   |\n        |\n        |\n        |\n       ---\n",
-					"_______\n|   |  \\|\n    O   |\n   \\|   |\n    |   |\n        |\n        |\n        |\n       ---\n",
-					"_______\n|   |  \\|\n    O   |\n   \\|/  |\n    |   |\n        |\n        |\n        |\n       ---\n",
-					"_______\n|   |  \\|\n    O   |\n   \\|/  |\n    |   |\n   / \\  |\n        |\n        |\n       ---\n",
+picture: .asciiz 	"_______\n|   |  \\|\n        |\n        |\n        |  ",
+					"\n        |\n        |\n        |\n       ---\n",
+					"_______\n|   |  \\|\n    O   |\n        |\n        |  ",
+					"\n        |\n        |\n        |\n       ---\n",
+					"_______\n|   |  \\|\n    O   |\n    |   |\n    |   |  ",
+					"\n        |\n        |\n        |\n       ---\n",
+					"_______\n|   |  \\|\n    O   |\n   \\|   |\n    |   |  ",
+					"\n        |\n        |\n        |\n       ---\n",
+					"_______\n|   |  \\|\n    O   |\n   \\|/  |\n    |   |  ",
+					"\n        |\n        |\n        |\n       ---\n",
+					"_______\n|   |  \\|\n    O   |\n   \\|/  |\n    |   |  ",
+					"\n   / \\  |\n        |\n        |\n       ---\n",
 
 clearScreen: .asciiz "[2J"
+
+sampleword:  .asciiz "s _ s t _ m d"
 .text
 
 main:
@@ -39,11 +47,18 @@ drawMan:			# Expects $s0 to hold the number of turns taken.
 	la $a0, clearScreen 	# Ascii escape sequence to clear screen
 	syscall
 
-	li $t1, 90 				# Each hangman guy is exactly 90 characters long
+	li $t1, 93 				# Each hangman guy is exactly 93 characters long
 	mul $t0, $s0, $t1 		# Multiply it by the current number of moved used
 	li $v0, 4 				# Print a string
-	la $a0, picture 		# current hangman picture
-	addu $a0, $a0, $t0
+	la $a0, picture 		# top half of the hangman picture
+	addu $a0, $a0, $t0 		# plus our offset for the number of moves taken
+	syscall
+	addi $t2, $a0, 50 		# Calculate the bottom half of our hangman picture, save it in temp.
+	
+	la $a0, sampleword 		# Print the current status of the word
+	syscall
+
+	move $a0, $t2 			# Get back the bottom half of our hangman picture from temp
 	syscall
 	jr $ra
 
