d43e584945d8951afbb3b34112accbfeba643670
Michael Aldridge <aldridge.mac@gmail.com>
2015-04-27 16:34:28 -0500

    valhala rampage through indentation and whitespace

--- main.asm
+++ main.asm
@@ -1,28 +1,28 @@
 .data
 # File
-	Prompt_Filename:	.asciiz "Enter the location of the dictionary file.\n"
-	Incorrect_Filename:	.asciiz "File does not exist. Try again.\n"
-	fileName:			.asciiz ""		# Name of dictionary file
-	inputSize:			.word	1024	# Maximum length of input filename
-	fileBuffer:			.space	1024	# Reserve 1024 bytes for the file buffer
+Prompt_Filename:	.asciiz "Enter the location of the dictionary file.\n"
+Incorrect_Filename:	.asciiz "File does not exist. Try again.\n"
+fileName:		.asciiz ""	# Name of dictionary file
+inputSize:		.word	1024	# Maximum length of input filename
+fileBuffer:		.space	1024	# Reserve 1024 bytes for the file buffer
 
 # Pictures
-picture: .asciiz 	"_______\n|   |  \\|\n        |\n        |\n        |  ",
-					"\n        |\n        |\n        |\n       ---\n",
-					"_______\n|   |  \\|\n    O   |\n        |\n        |  ",
-					"\n        |\n        |\n        |\n       ---\n",
-					"_______\n|   |  \\|\n    O   |\n    |   |\n    |   |  ",
-					"\n        |\n        |\n        |\n       ---\n",
-					"_______\n|   |  \\|\n    O   |\n   \\|   |\n    |   |  ",
-					"\n        |\n        |\n        |\n       ---\n",
-					"_______\n|   |  \\|\n    O   |\n   \\|/  |\n    |   |  ",
-					"\n        |\n        |\n        |\n       ---\n",
-					"_______\n|   |  \\|\n    O   |\n   \\|/  |\n    |   |  ",
-					"\n   / \\  |\n        |\n        |\n       ---\n",
+picture: 	.asciiz "_______\n|   |  \\|\n        |\n        |\n        |  ",
+			"\n        |\n        |\n        |\n       ---\n",
+			"_______\n|   |  \\|\n    O   |\n        |\n        |  ",
+			"\n        |\n        |\n        |\n       ---\n",
+			"_______\n|   |  \\|\n    O   |\n    |   |\n    |   |  ",
+			"\n        |\n        |\n        |\n       ---\n",
+			"_______\n|   |  \\|\n    O   |\n   \\|   |\n    |   |  ",
+			"\n        |\n        |\n        |\n       ---\n",
+			"_______\n|   |  \\|\n    O   |\n   \\|/  |\n    |   |  ",
+			"\n        |\n        |\n        |\n       ---\n",
+			"_______\n|   |  \\|\n    O   |\n   \\|/  |\n    |   |  ",
+			"\n   / \\  |\n        |\n        |\n       ---\n",
 
-clearScreen: .asciiz "[2J"
-underscore:  .asciiz "_"
-sampleword:  .asciiz "s _ s t _ m d"
+clearScreen:	.asciiz "[2J"
+underscore:  	.asciiz "_"
+sampleword:  	.asciiz "s _ s t _ m d"
 
 #Word bank
 Word1:		.asciiz "jazz"
@@ -90,130 +90,122 @@ Guess:		.asciiz "Guess a letter.\n"
 Correct_Word:	.asciiz "The correct word was:\n"
 NewLine:	.asciiz "\n"
 
-Goodbye:	.byte	#addresses?
+Goodbye:	.byte		#addresses?
 
 Guessed:	.space	32	#guessed letters
 
-GuessSoFar:	.space 24 	#s _ s t e _ d
+GuessSoFar:	.space	24 	#s _ s t e _ d
 .text
 
 ###############################################################################
 # Begin program
 .text
 main:
-	jal openFile
-	jal init
-	jal runGame
+	jal 	openFile
+	jal 	init
+	jal 	runGame
 
 #----------------------------------------------------------
 incorrectInput:	# File did not exist
-	li $v0, 4		# 4 is function code for printing a string
-	la $a0, Incorrect_Filename	# Load string into a0
-	syscall # Print the message
+	li 	$v0, 4			# 4 is function code for printing a string
+	la 	$a0, Incorrect_Filename	# Load string into a0
+	syscall 			# Print the message
 	
 #	Open the file in filename in read mode
 #	Read the first word of the file
 openFile:
-	getFileName:	# Get file path from user
-		# Display prompt
-		li $v0, 4			# 4 is function code for printing a string
-		la $a0, Prompt_Filename	# Load string into a0
-		syscall	# Print the prompt
-		
-		# Get user input
-		li $v0, 8			# 8 is function code for reading a string
-		la $a0, fileName	# Load fileName into a0
-		lw $a1, inputSize	# Load contents of inputSize into a1
-		syscall				# Input now stored in fileName
+getFileName:			# Get file path from user
+	# Display prompt
+	li 	$v0, 4			# 4 is function code for printing a string
+	la 	$a0, Prompt_Filename	# Load string into a0
+	syscall				# Print the prompt
 
-		sanitizeFileName:	# Fix the input
-    		li $t0, 0       #loop counter
-    		lw $t1, inputSize      #loop end
-		clean:
-			beq $t0, $t1, openFile_Read
-			lb $t3, fileName($t0)
-			bne $t3, 0x0a, increment_t0
-			sb $zero, fileName($t0)
-			increment_t0:
-				addi $t0, $t0, 1
-				j clean
+	# Get user input
+	li 	$v0, 8		# 8 is function code for reading a string
+	la 	$a0, fileName	# Load fileName into a0
+	lw 	$a1, inputSize	# Load contents of inputSize into a1
+	syscall			# Input now stored in fileName
 
-	openFile_Read:	# Open file for reading
-		li $v0, 13			# 13 is function code for opening a file
-		la $a0, fileName	# fileName is the name of the file
-		li $a1, 0			# Open for reading (flags are 0: read, 1: write)
-		li $a2, 0			# ignore the mode
-		syscall	# file descriptor returned in v0
-		move $s7, $v0			# Store the file descriptor in s7
-		
-#	printFileName:	# Print the entered file name
-#		li $v0, 4			# 4 is function code for printing a string
-#		la $a0, fileName	# load fileName into a0
-#		syscall	# Print the filename
-		
-#	displayFileDescriptor:	# Show the file descriptor
-#		li $v0, 1			# 1 is function for printing an int
-#		move $a0, $s7			# set a0 = s7 = file descriptor
-#		syscall
-		
-	checkFileValidity:	# Make sure the file opened correctly
-		li $t0, -1			# Set t0 = -1
-		beq $s7, $t0, incorrectInput	# If descriptor = -1, then jump back and re-get input
+sanitizeFileName:		# Fix the input
+	li 	$t0, 0       	#loop counter
+	lw 	$t1, inputSize	#loop end
+clean:
+	beq 	$t0, $t1, openFile_Read
+	lb 	$t3, fileName($t0)
+	bne 	$t3, 0x0a, increment_t0
+	sb 	$zero, fileName($t0)
+increment_t0:
+	addi 	$t0, $t0, 1
+	j 	clean
 
-	jr $ra	# Return to caller
+openFile_Read:	# Open file for reading
+	li 	$v0, 13		# 13 is function code for opening a file
+	la 	$a0, fileName	# fileName is the name of the file
+	li 	$a1, 0		# Open for reading (flags are 0: read, 1: write)
+	li 	$a2, 0		# ignore the mode
+	syscall			# file descriptor returned in v0
+	move 	$s7, $v0	# Store the file descriptor in s7
+		
+checkFileValidity:	# Make sure the file opened correctly
+	li 	$t0, -1				# Set t0 = -1
+	beq 	$s7, $t0, incorrectInput	# If descriptor = -1, then jump back and re-get input
+
+	jr 	$ra				# Return to caller
 	
 #	End openFile
 #------------------------------------------------------------------------
 
 init:
-	li $s0, 0		# $s0 will hold the number of turns taken.
-	jr $ra
+	li 	$s0, 0		# $s0 will hold the number of turns taken.
+	jr 	$ra
 	
 runGame:
-	jal prompt_Character 		#Ask for a character
-	move $a2, $v0
-	la $a1, Word50 				# We need to replace Word50 with the proper word
-	la $a0, Guessed
-	jal guessed_Update 			# make sure we have not previously guessed this.  
-	bne $v0, $0, wordDoesContain 	# Continue as if it was a correct answer
-	la $a3, GuessSoFar
-	jal generateWordToDisplay 		# Will return _ _ _ A _ B _ C
-	jal string_Contains 			# test for correctness
-	beq $v0, $0, doesNotContain
+	jal 	prompt_Character 		#Ask for a character
+	move 	$a2, $v0
+	la 	$a1, Word50 			# We need to replace Word50 with the proper word
+	la 	$a0, Guessed
+	jal 	guessed_Update 			# make sure we have not previously guessed this
+	bne 	$v0, $0, wordDoesContain 	# Continue as if it was a correct answer
+	la 	$a3, GuessSoFar
+	jal 	generateWordToDisplay 		# Will return _ _ _ A _ B _ C
+	jal 	string_Contains 		# test for correctness
+	beq 	$v0, $0, doesNotContain
+
 wordDoesContain: 				#correct
-	jal drawMan
-	j runGame
+	jal 	drawMan
+	j 	runGame
+
 doesNotContain: 				#possibly incorrect
 	# if incorrect:
-	addiu $s0, $s0, 1  			#Increment incorrect guesses
-	jal drawMan
-	beq $s0, 5, outOfGuesses
-	j runGame
+	addiu 	$s0, $s0, 1  			#Increment incorrect guesses
+	jal 	drawMan
+	beq 	$s0, 5, outOfGuesses
+	j 	runGame
 
 drawMan:			# Expects $s0 to hold the number of turns taken.
 	
-	li $v0, 11 		# Print a character
-	li $a0, 0x1b  	# Ascii excape
+	li 	$v0, 11 			# Print a character
+	li 	$a0, 0x1b  			# Ascii escape
 	syscall
 
-	li $v0, 4 				# Print a string
-	la $a0, clearScreen 	# Ascii escape sequence to clear screen
+	li 	$v0, 4 				# Print a string
+	la 	$a0, clearScreen 		# Ascii escape sequence to clear screen
 	syscall
 
-	li $t1, 93 				# Each hangman guy is exactly 93 characters long
-	mul $t0, $s0, $t1 		# Multiply it by the current number of moved used
-	li $v0, 4 				# Print a string
-	la $a0, picture 		# top half of the hangman picture
-	addu $a0, $a0, $t0 		# plus our offset for the number of moves taken
+	li 	$t1, 93 			# Each hangman guy is exactly 93 characters long
+	mul 	$t0, $s0, $t1 			# Multiply it by the current number of moved used
+	li 	$v0, 4 				# Print a string
+	la 	$a0, picture 			# top half of the hangman picture
+	addu 	$a0, $a0, $t0 			# plus our offset for the number of moves taken
 	syscall
-	addi $t2, $a0, 50 		# Calculate the bottom half of our hangman picture, save it in temp.
+	addi 	$t2, $a0, 50 			# Calculate the bottom half of our hangman picture, save it in temp.
 	
-	la $a0, GuessSoFar 		# Print the current status of the word
+	la 	$a0, GuessSoFar 		# Print the current status of the word
 	syscall
 
-	move $a0, $t2 			# Get back the bottom half of our hangman picture from temp
+	move 	$a0, $t2 			# Get back the bottom half of our hangman picture from temp
 	syscall
-	jr $ra
+	jr 	$ra
 
 outOfGuesses:
 	#:(
@@ -221,27 +213,27 @@ outOfGuesses:
 #----------------------------------------------------------
 #	prompt character
 prompt_Character:
-	addi $sp, $sp, -12		#allocate 
-	sw $ra, 0($sp)			#store old ra
-	sw $a0, 4($sp)			#store old a0
-	sw $s0, 8($sp)			#store old s0
+	addi 	$sp, $sp, -12		#allocate
+	sw 	$ra, 0($sp)		#store old ra
+	sw 	$a0, 4($sp)		#store old a0
+	sw 	$s0, 8($sp)		#store old s0
 	
-	li $v0, 12			#print string syscall
+	li 	$v0, 12			#print string syscall
 	syscall				#v0 contains a character
 	
-	move $s0, $v0			#character saved temporarily
+	move 	$s0, $v0		#character saved temporarily
 	
-	la $a0, NewLine			
-	jal print			#print new line
-	jal print			#print new line
+	la 	$a0, NewLine
+	jal 	print			#print new line
+	jal 	print			#print new line
 	
-	move $v0, $s0			#character back in return register
+	move 	$v0, $s0		#character back in return register
 	
-	lw $ra, 0($sp)			#load old ra
-	lw $a0, 4($sp)			#load old a0
-	lw $s0, 8($sp)			#load old s0
-	addi $sp, $sp, 12		#deallocate
-	jr $ra				#return
+	lw	$ra, 0($sp)		#load old ra
+	lw 	$a0, 4($sp)		#load old a0
+	lw 	$s0, 8($sp)		#load old s0
+	addi 	$sp, $sp, 12		#deallocate
+	jr 	$ra			#return
 	
 #	end prompt character
 #----------------------------------------------------------
@@ -250,26 +242,24 @@ prompt_Character:
 #	check to see if a string contains a given character
 
 string_Contains:
-	addi $sp, $sp, -4			#allocate 4 bytes
-
-	sw $a1, 0($sp)				#store old a0
-	
-	li $v0, 0			#set $v0 to 0 or false
+	addi 	$sp, $sp, -4	#allocate 4 bytes
+	sw 	$a1, 0($sp)	#store old a0
+	li 	$v0, 0		#set $v0 to 0 or false
 
 string_Contains_Loop:
-	lb $t0, 0($a1)				#load character in from string
-	beq $t0, $0, string_Contains_Loop_End	#stop loop if end of string is reached
-	beq $t0, $a2, char_Found		#branch if character matches
-	addi $a1, $a1, 1			#increment string address to continue scanning
-	j string_Contains_Loop			#jump to top of loop 
+	lb 	$t0, 0($a1)				#load character in from string
+	beq 	$t0, $0, string_Contains_Loop_End	#stop loop if end of string is reached
+	beq 	$t0, $a2, char_Found			#branch if character matches
+	addi 	$a1, $a1, 1				#increment string address to continue scanning
+	j 	string_Contains_Loop			#jump to top of loop
 	
 char_Found:
-	li $v0, 1				#if character found return value = 1
+	li 	$v0, 1		#if character found return value = 1
 
 string_Contains_Loop_End:
-	lw $a1, 0($sp)				#load old a0
-	addi $sp, $sp, 4			#deallocate
-	jr $ra					#return 
+	lw 	$a1, 0($sp)	#load old a0
+	addi 	$sp, $sp, 4	#deallocate
+	jr 	$ra		#return
 
 #	end character check
 #----------------------------------------------------------
@@ -278,10 +268,10 @@ string_Contains_Loop_End:
 #	guessed letter
 
 guessed_Update:
-	addi $sp, $sp, -8			#allocate 4 bytes
-	sw $a1, 0($sp)				#store old a0
-	sw $a0, 4($sp)				#store old a1
-	li $v0, 0 					#Whether or not it was found
+	addi 	$sp, $sp, -8			#allocate 4 bytes
+	sw 	$a1, 0($sp)			#store old a0
+	sw 	$a0, 4($sp)			#store old a1
+	li 	$v0, 0 				#Whether or not it was found
 
 guessed_Update_Loop:
 	lb $t0, 0($a0)				#load character from string
@@ -308,10 +298,10 @@ guessed_Update_Loop_End:
 #	print string
 
 print:
-	li $v0, 4				#print string
+	li 	$v0, 4				#print string
 	syscall
 	
-	jr $ra
+	jr 	$ra
 
 #	end print string
 #----------------------------------------------------------
@@ -320,50 +310,50 @@ print:
 #	print number
 
 print_num:
-	li $v0, 1				#print number
+	li 	$v0, 1				#print number
 	syscall
 	
-	jr $ra
+	jr 	$ra
 
 #	end print number
 #----------------------------------------------------------
 
 
 generateWordToDisplay:
-	addi $sp, $sp, -12			#allocate 4 bytes
-	sw $a0, 0($sp)				#store old a0
-	sw $a1, 4($sp)				#store old a1
-	sw $a3, 8($sp)
-	li $v0, 0 					#Whether or not it was found
-	move $t1, $a0
-	lb $t3, underscore
+	addi 	$sp, $sp, -12		#allocate 4 bytes
+	sw 	$a0, 0($sp)		#store old a0
+	sw 	$a1, 4($sp)		#store old a1
+	sw 	$a3, 8($sp)
+	li 	$v0, 0 			#Whether or not it was found
+	move 	$t1, $a0
+	lb 	$t3, underscore
 
 generateWordToDisplayLoop:
-	lb $t2, 0($a1)				# Fully correct word
-	lb $t0, 0($a0) 				# Every guessed letter
-	beq $t0, $0, generateWordToDisplayEOW 	 #stop loop if its the end on string
-	beq $t0, $t2, addLetter 				# if one of our guesses is correct
+	lb 	$t2, 0($a1)				# Fully correct word
+	lb 	$t0, 0($a0) 				# Every guessed letter
+	beq 	$t0, $0, generateWordToDisplayEOW 	# stop loop if its the end on string
+	beq 	$t0, $t2, addLetter 			# if one of our guesses is correct
 generateWordToDisplayLoopContinue:
-	sb $t3, 0($a3)
-	addi $a0, $a0, 1
-	j generateWordToDisplayLoop
+	sb 	$t3, 0($a3)
+	addi 	$a0, $a0, 1
+	j 	generateWordToDisplayLoop
 
 addLetter:
-	move $t3, $t2
-	j generateWordToDisplayLoopContinue
+	move 	$t3, $t2
+	j 	generateWordToDisplayLoopContinue
 
 generateWordToDisplayEOW:
-	addi $a3, $a3, 1 			# go to the next location in our word to display
-	move $a0, $t1 				# go to the beginning of our guessed letters
-	addi $a1, $a1, 1 			# go to the next letter in our fully correct word
-	lb $t5, 0($a1)
-	lb $t3, underscore
-	beq $t5, $0 generateWordToDisplayEND
-	j generateWordToDisplayLoop
+	addi 	$a3, $a3, 1 			# go to the next location in our word to display
+	move 	$a0, $t1 			# go to the beginning of our guessed letters
+	addi 	$a1, $a1, 1 			# go to the next letter in our fully correct word
+	lb 	$t5, 0($a1)
+	lb 	$t3, underscore
+	beq	$t5, $0 generateWordToDisplayEND
+	j 	generateWordToDisplayLoop
 
 generateWordToDisplayEND:
-	lw $a3, 8($sp)
-	lw $a1, 4($sp)				#load old a1
-	lw $a0, 0($sp)				#load old a0
-	addi $sp, $sp, 12			#deallocate
-	jr $ra					#return
+	lw 	$a3, 8($sp)
+	lw 	$a1, 4($sp)	#load old a1
+	lw 	$a0, 0($sp)	#load old a0
+	addi 	$sp, $sp, 12	#deallocate
+	jr 	$ra		#return
