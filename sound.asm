5a758edac46a163d78209618ff7f645d41fa4010
Michael Aldridge <aldridge.mac@gmail.com>
2015-04-28 02:48:08 -0500

    finalize sounds

--- sound.asm
+++ sound.asm
@@ -1,17 +1,16 @@
 .data
-debugstr:	.asciiz "Debug Split\n"
+debugstr:		.asciiz "Debug Split\n"
 
-.text
-main:	
-	jal	volumeVoice
+	.text
+main:
 	jal 	rightSound
 	jal	debug
-	jal	volumeVoice
 	jal	wrongSound
 	jal	debug
-	jal	volumeVoice
 	jal	winSound
-	
+	jal 	debug
+	jal	loseSound
+
 	li	$v0, 10
 	syscall
 
@@ -19,90 +18,121 @@ debug:
 	li	$v0, 4
 	la	$a0, debugstr
 	syscall
+
+	li	$v0, 32
+	li	$a0, 1000
+	syscall
+
 	jr	$ra
 
-volumeVoice:
+rightSound:
 	li	$a2, 0	# instrument ID
 	li	$a3, 127# volume
 	li	$v0, 33
-	jr	$ra
-	
-rightSound:
-	li	$a0, 69
-	li	$a1, 250
+
+	li	$a0, 55
+	li	$a1, 100
 	syscall
 
-	li	$a0, 79
-	li	$a1, 250
+	li	$v0, 32
+	li	$a0, 65
 	syscall
 
-	li	$a0, 89
-	li	$a1, 250
+	li	$a2, 0	# instrument ID
+	li	$a3, 127# volume
+	li	$v0, 33
+
+	li	$a0, 60
+	li	$a1, 100
 	syscall
-	
+
 	jr $ra
 
 wrongSound:
+	li	$a2, 0	# instrument ID
+	li	$a3, 127# volume
+	li	$v0, 33
 
-	li	$a0, 89
-	li	$a1, 250
+	li	$a0, 60
+	li	$a1, 100
 	syscall
 
-	li	$a0, 79
-	li	$a1, 250
+	li	$v0, 32
+	li	$a0, 65
 	syscall
-	
-	li	$a0, 69
-	li	$a1, 250
+
+	li	$a2, 0	# instrument ID
+	li	$a3, 127# volume
+	li	$v0, 33
+
+	li	$a0, 55
+	li	$a1, 100
 	syscall
 
 	jr $ra
 
 winSound:
+	li	$a2, 0	# instrument ID
+	li	$a3, 127# volume
+	li	$v0, 33
 
 	li	$a0, 69
-	li	$a1, 250
+	li	$a1, 100
 	syscall
 
-	li	$a0, 79
-	li	$a1, 250
-	syscall
-	
-	li	$a0, 89
-	li	$a1, 250
-	syscall
-
-	li	$a0, 79
-	li	$a1, 250
+	li	$a0, 75
+	li	$a1, 100
 	syscall
 
 	li	$a0, 89
+	li	$a1, 100
+	syscall
+
+	li	$a0, 78
 	li	$a1, 250
 	syscall
 
-	li	$a0, 99
+	li	$a0, 85
+	li	$a1, 100
+	syscall
+
+	li	$a0, 90
 	li	$a1, 250
 	syscall
 
-	li	$a0, 89
+	li	$a0, 85
 	li	$a1, 250
 	syscall
 
-	li	$a0, 99
+	li	$a0, 90
 	li	$a1, 250
 	syscall
 
-	li	$a0, 109
-	li	$a1, 250
+	li	$a0, 94
+	li	$a1, 500
 	syscall
-
-	li	$a0, 119
-	li	$a1, 250
-	syscall
-
-	li	$a0, 120
-	li	$a1, 250
-	syscall
-
 
 	jr $ra
+
+loseSound:
+	li	$a2, 0	# instrument ID
+	li	$a3, 127# volume
+	li	$v0, 33
+
+	li	$a0, 55
+	li	$a1, 500
+	syscall
+
+	li	$a0, 54
+	li	$a1, 500
+	syscall
+
+	li	$a0, 51
+	li	$a1, 500
+	syscall
+
+	li	$a0, 50
+	li	$a1, 1500
+	syscall
+
+	jr	$ra
